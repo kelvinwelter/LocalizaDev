240b021e18f3028a7f7bb9849a5a269b3aba4b7b
Kelvin Welter <kelvinwelter.b@gmail.com>
2020-01-17 20:49:38 -0300

    Initial Commit

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,70 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index (request, response) {
+        const devs = await Dev.find();
+        return response.json(devs);
+    },
+
+    async store (request, response) {
+        const { github_username, techs, latitude, longitude } = request.body;
+
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+            const { name = login, avatar_url, bio } = apiResponse.data;
+        
+            const techsArray = parseStringAsArray(techs)
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            }
+            
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            })
+        }
+    
+        return response.json(dev);
+    },
+
+    async update(request, response) {
+        const { github_username } = request.params;
+        const { techs, latitude, longitude, name, avatar_url, bio } = request.body;
+
+        const update = {
+            techs,
+            latitude,
+            longitude,
+            name, 
+            avatar_url,
+            bio,
+        };
+
+        for (let param in update) if (!update[param]) delete update[param];
+
+        const dev = await Dev.findOneAndUpdate({ github_username }, update, {
+            new: true,
+        });
+
+        return response.json(dev);
+    },
+
+    async destroy(request, response) {
+        const { github_username } = request.params;
+
+        const dev = await Dev.findOneAndDelete({ github_username });
+
+        return response.json(dev);
+    }
+};
